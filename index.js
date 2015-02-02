2b81c112a2d4cb460e9232fb20d8aeedcb91fc8c
Thorsten Lorenz <thlorenz@gmx.de>
2015-02-01 21:55:09 -0500

    refactor: created sample mapper prototype to separate code into smaller functions
    - fixed bug related to samples
    - now seeing links in chrome://tracing but encountering error when clicking it
    - error is inside Tree.getCallTreeNode: "Uncaught TypeError: Cannot read property 'addChild' of undefined"

--- index.js
+++ index.js
@@ -23,153 +23,198 @@ function getParentIds(frames, node) {
   return acc;
 }
 
-var inside = {};
+function SampleMapper(frames, startTime, endTime, samples, name, pid, tid, cpu) {
+  if (!(this instanceof SampleMapper)) return new SampleMapper(frames, startTime, endTime, samples, name, pid, tid, cpu);
 
-function mapSamples(frames, startTime, endTime, samples, name, pid, tid, cpu) {
-  var samplesLen = samples.length
-    , infos      = [] 
-    , events     = []
-    , prevId     = -1
-    , prevInfo   = null
-    , i
+  this._frames       = frames;
+  this._startTime    = startTime;
+  this._endTime      = endTime;
+  this._samplesIn    = samples;
+  this._samplesInLen = this._samplesIn.length;
+  this._tickLen      = (this._endTime - this._startTime) / this._samplesInLen
 
+  this._name = name;
+  this._pid  = pid;
+  this._tid  = tid;
+  this._cpu  = cpu;
 
+  this._inside      = {};
+  this._infos       = []
+  this._events      = []
+  this._prevInfo    = null
+  this._currentTick = 0
+  this._samples         = []
+
+  this._previousFunctionExit = undefined
+  this._previousFunctionId = undefined;
+}
+
+var proto = SampleMapper.prototype;
+
+proto.map = function map() {
+  this._consolidateSamples();
+  return this._processInfos();
+}
+
+proto._consolidateSamples = function _consolidateSamples() {
   // Consolidate cases in which the same script ID appears in a row
   // In that case we'll increase it's ticks count
-  for (i = 0; i < samplesLen; i++) {
-    if (samples[i] === prevId) {
+  var prevId = -1, prevInfo;
+  for (var i = 0; i < this._samplesInLen; i++) {
+    if (this._samplesIn[i] === prevId) {
       prevInfo.ticks++;
     } else {
-      prevId = samples[i];
+      prevId = this._samplesIn[i];
       prevInfo = { id: prevId, ticks: 1 };
-      infos.push(prevInfo);
+      this._infos.push(prevInfo);
     }
   }
+}
 
-  var samps  = [] 
-    , tickLen = (endTime - startTime) / samplesLen
-    , currentTick = 0
-    , previousFunctionExit
-    , previousFunctionId;
-
-  for (i = 0; i < infos.length; i++) {
-    var ts         = startTime + (tickLen * currentTick)
-      , info       = infos[i]
-      , stackFrame = frames[info.id]
-      , j 
-
-    samps.push({
-        name   : name
-      , weight : info.ticks * tickLen
-      , ts     : ts 
-      , sf     : infos.id
-      , tid    : tid
-      , cpu    : cpu
+proto._addSample = function _addSample(info, timestamp) {
+    this._samples.push({
+        name   : this._name
+      , weight : info.ticks * this._tickLen
+      , ts     : timestamp 
+      , sf     : info.id
+      , tid    : this._tid
+      , cpu    : this._cpu
     })
+}
 
-    // Get IDs of all functions that are parents to this function
-    var parentIds = getParentIds(frames, stackFrame);
-    
-    // Only if the previous leaf function is not part of the parents now did we exit it
-    if (previousFunctionExit && !~parentIds.indexOf(previousFunctionId)) {
-      events.push(previousFunctionExit)
-      inside[previousFunctionId] = false;
-    }
+proto._maybeExitPreviousFunction = function _maybeExitPreviousFunction(parentIds) {
 
-    if (info.id === 17) {
-      inspect(info)
-      inspect(parentIds)
-    }
+  // Only if the previous leaf function is not part of the parents anymore did we actually exit it
+  if (this._previousFunctionExit && !~parentIds.indexOf(this._previousFunctionId)) {
+    this._events.push(this._previousFunctionExit)
+    this._inside[this._previousFunctionId] = false;
+  }
+}
 
-    // Some functions were part of the stack of the previous function, but aren't part 
-    // of the stack of the current one.
-    // Therefore we need to emit exit events for them.
-    var insideKeys = Object.keys(inside), id, k
-    for (j = 0; j < insideKeys.length; j++) {
-      k = insideKeys[j];
-      id = parseInt(k);
-      if (inside[k] && id !== info.id && k !== previousFunctionId && !~parentIds.indexOf(id)) {
-        events.push({
-            name : frames[k].name
-          , cat  : name 
-          , ph   : 'E'
-          , pid  : pid
-          , tid  : tid
-          , ts   : previousFunctionExit.ts
-          , args : {}
-        })
+proto._exitParentsNowOffStack = function _exitParentsNowOffStack(parentIds, info) {
+  // Some functions were part of the stack of the previous function, but aren't part 
+  // of the stack of the current one.
+  // Therefore we need to emit exit events for them.
 
-        inside[k] = false;
-      }
-    }
-
-    // We might have entered parent functions that called the current function
-    // Ensure to raise an event that they have been entered
-    for (j = parentIds.length - 1; j >= 0; j--) {
-      id = parentIds[j];
-      if (!inside[id]) {
-        events.push({
-            name : frames[id].name
-          , cat  : name 
-          , ph   : 'B'
-          , pid  : pid
-          , tid  : tid
-          , ts   : ts
-          , args : {}
-        })
-        inside[id] = true;
-      }
-    }
-
-    // Enter current function
-    if (!inside[info.id]) {
-        events.push({
-          name : stackFrame.name
-        , cat  : name
-        , ph   : 'B'
-        , pid  : pid
-        , tid  : tid
-        , ts   : ts
+  var insideKeys = Object.keys(this._inside), id, k
+  for (var j = 0; j < insideKeys.length; j++) {
+    k = insideKeys[j];
+    id = parseInt(k);
+    if (this._inside[k] && id !== info.id && k !== this._previousFunctionId && !~parentIds.indexOf(id)) {
+      this._events.push({
+          name : this._frames[k].name
+        , cat  : this._name 
+        , ph   : 'E'
+        , pid  : this._pid
+        , tid  : this._tid
+        , ts   : this._previousFunctionExit.ts
         , args : {}
       })
-      inside[info.id] = true;
+
+      this._inside[k] = false;
+    }
+  }
+}
+
+proto._enterParentsNewOnStack = function _enterParentsNewOnStack(parentIds, timestamp) {
+
+  // We might have entered parent functions that called the current function
+  // Ensure to raise an event that they have been entered
+  for (var j = parentIds.length - 1; j >= 0; j--) {
+    var id = parentIds[j];
+    if (!this._inside[id]) {
+      this._events.push({
+          name : this._frames[id].name
+        , cat  : this._name 
+        , ph   : 'B'
+        , pid  : this._pid
+        , tid  : this._tid
+        , ts   : timestamp 
+        , args : {}
+      })
+      this._inside[id] = true;
+    }
+  }
+}
+
+proto._enterCurrentFunction = function _enterCurrentFunction(info, stackFrame, timestamp) {
+
+    // Enter current function
+    if (!this._inside[info.id]) {
+        this._events.push({
+          name : stackFrame.name
+        , cat  : this._name
+        , ph   : 'B'
+        , pid  : this._pid
+        , tid  : this._tid
+        , ts   : timestamp 
+        , args : {}
+      })
+      this._inside[info.id] = true;
     }
 
-    // Exit current function
-    previousFunctionExit = {
+    // Define exit event of current function to be used when processing next function
+    // @see: _maybeExitPrevousFunction
+    this._previousFunctionExit = {
         name : stackFrame.name
-      , cat  : name
+      , cat  : this._name
       , ph   : 'E'
-      , pid  : pid
-      , tid  : tid
-      , ts   : ts + (info.ticks * tickLen)
+      , pid  : this._pid
+      , tid  : this._tid
+      , ts   : timestamp + (info.ticks * this._tickLen)
       , args : {}
     }
-    previousFunctionId = info.id;
+
+    this._previousFunctionId = info.id;
+}
+
+proto._exitAllRemainingFunctions = function _exitAllRemainingFunctions() {
+  var insideKeys = Object.keys(this._inside), k;
+  for (var j = 0; j < insideKeys.length; j++) {
+    k = insideKeys[j];
+    if (this._inside[k]) {
+      this._events.push({
+          name : this._frames[k].name
+        , cat  : this._name 
+        , ph   : 'E'
+        , pid  : this._pid
+        , tid  : this._tid
+        , ts   : this._endTime 
+        , args : {}
+      })
+    }
+  }
+}
+
+proto._processInfos = function _processInfos() {
+  var currentTick = 0;
+
+  for (var i = 0; i < this._infos.length; i++) {
+    var ts         = this._startTime + (this._tickLen * currentTick)
+      , info       = this._infos[i]
+      , stackFrame = this._frames[info.id]
+      , j 
+
+    this._addSample(info, ts);
+
+    // Get IDs of all functions that are parents to this function
+    var parentIds = getParentIds(this._frames, stackFrame);
+    this._maybeExitPreviousFunction(parentIds);
+
+    this._exitParentsNowOffStack(parentIds, info);
+    
+    this._enterParentsNewOnStack(parentIds, ts);
+
+    this._enterCurrentFunction(info, stackFrame, ts);
     
     currentTick += info.ticks;
   }
 
   // Finally exit all functions we are still in after all is finished
-  Object.keys(inside).forEach(function (k) {
-    if (inside[k]) {
-      events.push({
-          name : frames[k].name
-        , cat  : name 
-        , ph   : 'E'
-        , pid  : pid
-        , tid  : tid
-        , ts   : endTime 
-        , args : {}
-      })
+  this._exitAllRemainingFunctions();
 
-      inside[k] = false;
-    }
-  })
-
-  return { samples: samps, events: events };
+  return { samples: this._samples, events: this._events };
 }
-
 
 function inspect(obj, depth) {
   console.error(require('util').inspect(obj, false, depth || 5, true));
@@ -179,21 +224,22 @@ var fs = require('fs');
 
 var cpuprofileJSON = fs.readFileSync(require.resolve('./test/fixtures/fibonacci.cpuprofile'), 'utf8');
 var cpuprofile = JSON.parse(cpuprofileJSON);
-var category = 'iojs';
-var root = cpuprofile.head;
-var stackFrames = { };
+var category = 'iojs'
+  , stackFrames = { };
 
-addFrame(stackFrames, category, root)
+addFrame(stackFrames, category, cpuprofile.head)
 
-var mapped = mapSamples(
+var mapped = new SampleMapper(
     stackFrames
   , parseFloat(cpuprofile.startTime) * 1000000.0
   , parseFloat(cpuprofile.endTime) * 1000000.0
   , cpuprofile.samples
-  , 'iojs'
+  , category 
   , 2
   , 1
-  , 0);
+  , 0
+).map()
+
 
 var res = {
     traceEvents : mapped.events
